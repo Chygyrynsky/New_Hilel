7a43cfcda17d63b44160af88945a41e89d19b8cc
Chygyrynsky <chygyrynsky@gmail.com>
2023-03-08 21:04:35 +0200

    home work 26

---
+++ index.js
@@ -0,0 +1,19 @@
+
+window.onload = function(){
+   
+document.getElementById("text").onfocus = function  ()  { 
+    let div = document.createElement('div');
+    document.body.append(div);
+}
+
+document.getElementById("text").onblur = function ()  { 
+    const square = document.querySelector("div");
+    square.remove();
+
+}
+
+}
+
+
+
+
